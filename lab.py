9060c256eb3a7befe26c8be307c71f1e696070fc
Michael Zhang <mzhang00@stuy.edu>
2020-03-11 23:31:17 -0400

    t

--- lab.py
+++ lab.py
@@ -9,8 +9,9 @@ fig = go.Figure(data=[go.Surface(
 )
 
 fig.update_traces(contours_z=dict(show=True, usecolormap=True,
-                                  highlightcolor="ice", project_z=True))
+                                  highlightcolor="limegreen", project_z=True))
 fig.update_layout(title='Equipotentials', autosize=False,
+                  colorway="blueviolet",
                   scene_camera_eye=dict(x=1.87, y=0.88, z=-0.64),
                   width=500, height=500,
                   margin=dict(l=65, r=50, b=65, t=90)
